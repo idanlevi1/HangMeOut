1b5bf9b323e87136032783449100caec92a95b9e
idanlevi1 <idanlevi2@gmail.com>
2019-07-05 18:45:56 +0300

    install share, gradient & awesome btn

--- App.js
+++ App.js
@@ -1,29 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 import I18n from 'react-native-i18n';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import LinearGradient from 'react-native-linear-gradient';
+import AwesomeButton from "react-native-really-awesome-button";
+import Share from 'react-native-share'
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  onShare = () => {
+    const options = {
+      title: 'Share via',
+      message: 'some message',
+      url: 'some share url',
+      social: Share.Social.WHATSAPP
+    }
+    Share.open(options)
+      .then((res) => { console.log(res) })
+      .catch((err) => { err && console.log(err); });
+  }
+
   render() {
     const systemVersion = DeviceInfo.getSystemVersion();
     const appName = DeviceInfo.getApplicationName()
-    const deviceCountry = DeviceInfo.getDeviceCountry(); 
+    const deviceCountry = DeviceInfo.getDeviceCountry();
     const deviceName = DeviceInfo.getDeviceName();
     const carrier = DeviceInfo.getCarrier() || '-';
     I18n.translations = {
@@ -34,16 +35,17 @@ export default class App extends Component<Props> {
         greeting: 'Bonjour!',
       },
     };
+
     return (
-      <View style={styles.container}>
+      <LinearGradient colors={['#0c669f', '#0c8f6a', '#0c6a']} style={styles.linearGradient}>
         <Text style={styles.welcome}>Welcome to {appName}!</Text>
         <Text style={styles.instructions}>systemVersion: {systemVersion}</Text>
         <Text style={styles.instructions}>deviceCountry: {deviceCountry}</Text>
         <Text style={styles.instructions}>deviceName: {deviceName}</Text>
         <Text style={styles.instructions}>carrier: {carrier}</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
         <Text style={styles.instructions}>{I18n.t('greeting')}</Text>
-      </View>
+        <AwesomeButton onPress={this.onShare}>AwesomeButton - share</AwesomeButton>
+      </LinearGradient>
     );
   }
 }
@@ -64,5 +66,18 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  linearGradient: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+    backgroundColor: 'transparent',
   },
 });
