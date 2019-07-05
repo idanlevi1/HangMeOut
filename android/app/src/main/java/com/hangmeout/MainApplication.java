dd0dda99b5d6d99f89119c91ade18b1d971d3df8
idanlevi1 <idanlevi2@gmail.com>
2019-07-05 19:09:43 +0300

    install camera & qrcode-scanner

--- android/app/src/main/java/com/hangmeout/MainApplication.java
+++ android/app/src/main/java/com/hangmeout/MainApplication.java
@@ -3,6 +3,7 @@ package com.hangmeout;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import cl.json.RNSharePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new RNSharePackage(),
             new LinearGradientPackage(),
             new RNI18nPackage(),
